51b7fd9a976277544a56d0a1caed0cf1b3ec84f7
Derly Q <dquejada1028@cue..edu.co>
2025-05-03 12:14:26 -0500

    Proyecto creado, con los dos modulos a desarrollar

---
+++ src/main/java/co/edu/apitienda/ApiTiendaApplication.java
@@ -0,0 +1,13 @@
+package co.edu.apitienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTiendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTiendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/apitienda/ApiTiendaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.apitienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTiendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
